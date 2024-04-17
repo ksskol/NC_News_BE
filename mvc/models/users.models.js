17f50cab7849a130c7ae8fe294db6bf5f6d600bb
ksskol <skolotakristina.com>
2024-04-17 17:06:52 +0100

    GET all users

---
+++ mvc/models/users.models.js
@@ -0,0 +1,10 @@
+const db = require("../../db/connection");
+
+function fetchUsers() {
+  return db.query(`SELECT * FROM users`)
+  .then(({ rows }) => {
+    return rows;
+  });
+}
+
+module.exports = { fetchUsers };
