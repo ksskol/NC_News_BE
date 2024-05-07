2d74e294055f713c82372434784c8b3b11a9d389
ksskol <skolotakristina.com>
2024-05-07 01:40:38 +0100

    allow cross origin resource sharing

--- mvc/app.js
+++ mvc/app.js
@@ -16,11 +16,14 @@ const { getUsers } = require("./controllers/users.controllers");
 const app = express();
 app.use(express.json());
 
+const cors = require('cors');
+app.use(cors());
+
 app.get("/api", getEndpoints);
 
 app.get("/api/topics", getTopics);
 
-app.get("/api/users/", getUsers);
+app.get("/api/users", getUsers);
 
 app.get("/api/articles", getArticles);
 app.get("/api/articles/:article_id", getArticlesById);
